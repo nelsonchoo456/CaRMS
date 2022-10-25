3760ffa899eaa087840bfd238f6290fecf7cd34d
Leo Hernando Tan <e0775385@u.nus.edu>
2022-10-25 11:23:11 +0800

    Added Model entity, added Category and Model to persistence unit. Have not developed logic tier EJB module.

--- CaRMSLibrary/src/entity/Category.java
+++ CaRMSLibrary/src/entity/Category.java
@@ -6,11 +6,13 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -25,8 +27,12 @@ public class Category implements Serializable {
     private Long categoryId;
     @Column(nullable = false, length = 32)
     private String categoryName;
+    
+    @OneToMany(mappedBy = "category")
+    private List<Model> models;
 
     public Category() {
+        this.models = new ArrayList<>();
     }
 
     public Category(Long categoryId, String categoryName) {
---
+++ CaRMSLibrary/src/entity/Model.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author lhern
+ */
+@Entity
+public class Model implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long modelId;
+    private String make;
+    
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private Category category;
+
+    public Model() {
+    }
+
+    public Model(String make) {
+        this.make = make;
+    }
+    
+    
+
+    public Long getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(Long modelId) {
+        this.modelId = modelId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (modelId != null ? modelId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the modelId fields are not set
+        if (!(object instanceof Model)) {
+            return false;
+        }
+        Model other = (Model) object;
+        if ((this.modelId == null && other.modelId != null) || (this.modelId != null && !this.modelId.equals(other.modelId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Model[ id=" + modelId + " ]";
+    }
+
+    /**
+     * @return the make
+     */
+    public String getMake() {
+        return make;
+    }
+
+    /**
+     * @param make the make to set
+     */
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    /**
+     * @return the category
+     */
+    public Category getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
+}
