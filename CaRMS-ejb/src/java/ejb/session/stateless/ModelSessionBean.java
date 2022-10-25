e6f3f8a9e169b37d0f85de19b68b2f5c43987acf
Leo Hernando Tan <e0775385@u.nus.edu>
2022-10-26 01:10:44 +0800

    Created ejb stateless session bean, Created stateless session bean for Model CreateNewModel() and ViewAllModels() methods are added but no Update or Delete methods yet

---
+++ CaRMS-ejb/src/java/ejb/session/stateless/ModelSessionBean.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import entity.Model;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author lhern
+ */
+@Stateless
+public class ModelSessionBean implements ModelSessionBeanRemote, ModelSessionBeanLocal {
+
+    @PersistenceContext(unitName = "CaRMS-ejbPU")
+    private EntityManager em;
+
+    
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    @Override
+    public Long createNewModel(Model model)
+    {
+        em.persist(model);
+        em.flush();
+        
+        return model.getModelId();
+    }
+    
+    
+    @Override
+    public List<Model> viewAllModels() 
+    {
+        Query query = em.createQuery("SELECT m FROM Model m");
+        
+        return query.getResultList();
+    }
+}
